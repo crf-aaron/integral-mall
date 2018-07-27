266be80d4047c6fbfcb7dd253f5a804791265aa2
成瑞峰 <937272305@qq.com>
2018-07-27 15:35:12 +0800

    积分商城静态页面完成

--- js/index.js
+++ js/index.js
@@ -8,9 +8,11 @@ $(function () {
     });
     $('.btn-sure').on('click',function () {
         $('#whetherExchange').toggle();
+        $('#whetherCancelOrder').toggle();
     });
     $('.btn-cancel').on('click',function () {
         $('#whetherExchange').toggle();
+        $('#whetherCancelOrder').toggle();
     });
     /*编辑收货地址*/
     $('.address').on('click',function () {
@@ -30,5 +32,56 @@ $(function () {
         $('.progress-value').width(proWidth);
     }
     progressVal('.pro-val',20);
+
+    /*签到*/
+    $('#signIn').on("click",function () {
+        $('#signInSuccess').toggle();
+        setTimeout(function () {
+            $('#signInSuccess').toggle();
+            window.location.href = './signIn.html';
+        },2000)
+    });
+    /*注册手机号*/
+    $('#phone').on('click',function () {
+        $('.phone-popup').toggle();
+    });
+    $('#close').on('click',function () {
+        $('.phone-popup').toggle();
+    });
+    /*兑换记录*/
+    $('#record li').on('click', function () {
+        var index = $(this).index();
+        $(this).siblings('li').removeClass('record-active');
+        $(this).addClass('record-active');
+        if(index == 1) {
+            $('.record-list').css('display','none');
+            $('.record-noReceive').css('display','block');
+        } else if(index == 2) {
+            $('.record-list').css('display','none');
+            $('.record-complete').css('display','block');
+        } else if(index == 3) {
+            $('.record-list').css('display','none');
+            $('.record-cancel').css('display','block');
+        } else {
+            $('.record-list').css('display','none');
+            $('.record-all').css('display','block');
+        }
+    });
+    /*取消订单*/
+    $('.btn-cancel-order').on('click',function () {
+        $('.confirm-popup').toggle();
+    });
+    /*订单详情*/
+    $('.btn-order-detail').on('click',function () {
+        window.location.href = './order-detail.html';
+    });
+    /*积分规则*/
+    $('.btn-rule').on('click',function () {
+        window.location.href = './rule.html';
+    });
+    /*我要拼团*/
+    $('.btn-collage').on('click',function () {
+        window.location.href = './collage-members.html';
+    });
 
 })
